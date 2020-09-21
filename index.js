9f157e7af1b4ab5a4244ff4dada6cbeb806f899f
Divyendu Singh <singh@prisma.io>
2020-09-21 14:12:56 +0200

    add comment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv')
+// const dotenv = require('dotenv').config()
 const ci = require('@prisma/ci-info')
 
 console.log(process.env.CI)
 console.log(ci.isCI, ci.name)
 dotenv.config() // This won't affect the output
-console.log(ci.isCI, ci.name)
+console.log(ci.isCI, ci.name) // ci.isCLI is still false, comment line 1 and uncomment line 2 to fix it i.e. dotenv.config() should be called before @prisma/ci-info import
